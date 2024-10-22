88aaf88048a3e892fc3fcdb3182c0c1f96f71b8f
xthanh12 <vuxuanthanh1203@gmail.com>
2024-10-22 18:05:10 +0700

    set up service

---
+++ src/routes.ts
@@ -0,0 +1,14 @@
+// import { verifyGatewayRequest } from '@vuxuanthanh1203/findjob-helpers-lib';
+import { Application } from 'express';
+// import { gigRoutes } from '@gig/routes/gig';
+// import { healthRoutes } from '@gig/routes/health';
+
+// const BASE_PATH = '/api/v1/gig';
+
+const appRoutes = (app: Application): void => {
+  app.use('', () => console.log('health route'));
+  // app.use('', healthRoutes());
+  // app.use(BASE_PATH, verifyGatewayRequest, gigRoutes());
+};
+
+export { appRoutes };
